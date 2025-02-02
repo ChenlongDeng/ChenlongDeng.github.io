bff9f246e5fe41ebc39b92657131c4ace94ec24f
Chenlong Deng <dengchenlong@ruc.edu.cn>
2025-02-02 21:58:16 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@ import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
