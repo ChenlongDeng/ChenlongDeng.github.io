ffed645ac99890848ec7385965dacd81deb70b27
Chenlong Deng <dengchenlong@ruc.edu.cn>
2025-02-04 17:32:06 +0800

    only codes in main branch

---
+++ _next/static/u3zM0_zu6VhxfQ9dO2qAV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a79c42dceb983b20.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
